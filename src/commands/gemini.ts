86d3577838ecdbf912cf20ebe4836fd075d306a9
cassie <codef53@users.noreply.github.com>
2025-05-24 00:57:47 -0600

    always 2.5 flash, variable thinking

--- src/commands/gemini.ts
+++ src/commands/gemini.ts
@@ -2,10 +2,17 @@ import type { GenerateContentConfig } from '@google/genai'
 import type { CommandInteraction, CreateApplicationCommandOptions } from 'oceanic.js'
 import { GoogleGenAI } from '@google/genai'
 import { env } from 'bun'
-import { ApplicationCommandOptionTypes, ApplicationCommandTypes } from 'oceanic.js'
+import { ApplicationCommandOptionTypes, ApplicationCommandTypes, Constants } from 'oceanic.js'
 
 const { GEMINI_KEY } = env
 const ai = new GoogleGenAI({ apiKey: GEMINI_KEY! })
+
+const PRO_USERS = env.PRO_USER_IDS!.split(',')
+// I really really hope people understand this is a joke...
+const NOT_PRO_MESSAGE = `# Whoops! <:whoops:1375727444199215124>
+You are not in the pro(💎) user list!
+Paypal fseusb@gmail.com \`$14.99\` and dm <@280411966126948353> your receipt :3
+-# dm cassie for terms & conditions~`
 
 export const description: CreateApplicationCommandOptions = {
   name: 'gemini',
@@ -16,49 +23,45 @@ export const description: CreateApplicationCommandOptions = {
       name: 'input',
       description: 'the input to gemini',
       type: ApplicationCommandOptionTypes.STRING,
-      minLength: 2,
-      maxLength: 1_000,
+      maxLength: 1024,
       required: true,
     },
     {
-      name: 'model',
-      description: 'the model to use',
-      type: ApplicationCommandOptionTypes.STRING,
-      choices: [
-        {
-          name: '2.0 flash (default)',
-          value: 'gemini-2.0-flash',
-        },
-        // TODO: implement ratelimiting to 10 per 24 hours
-        {
-          name: '2.5 flash (thinking)',
-          value: 'gemini-2.5-flash-preview-04-17',
-        },
-        // TODO: implement ratelimiting to 5 per 24 hours
-        // {
-        //   name: '2.5 pro',
-        //   value: 'gemini-2.5-pro-preview-05-06',
-        // },
-      ],
-      required: false,
+      name: 'thinking',
+      description: 'number of thinking tokens to use',
+      type: ApplicationCommandOptionTypes.NUMBER,
+      choices: Object.entries({
+        'none': 0,
+        'low': 1024,
+        'medium': 4096,
+        '💎 high ': 16384,
+      }).map(([name, value]) => ({ name: `${name} (${value})`, value })),
     },
   ],
 }
 
 export async function handler(interaction: CommandInteraction) {
-  interaction.defer()
-
   const input = interaction.data.options.getStringOption('input', true)
-  const model = interaction.data.options.getStringOption('model')?.value ?? 'gemini-2.0-flash'
+  const thinkingBudget = interaction.data.options.getNumberOption('thinking')?.value ?? 0
+
+  if (thinkingBudget > 4_096 && !PRO_USERS.includes(interaction.user.id)) {
+    return interaction.reply({
+      content: NOT_PRO_MESSAGE,
+      flags: Constants.MessageFlags.EPHEMERAL,
+    })
+  }
+
+  interaction.defer()
 
   const config: GenerateContentConfig = {
     systemInstruction: 'use discord markdown, try to keep your response concise and under 2000 characters',
+    thinkingConfig: {
+      includeThoughts: thinkingBudget > 0,
+      thinkingBudget,
+    },
   }
-  if (model !== 'gemini-2.0-flash')
-    config.thinkingConfig = { includeThoughts: true, thinkingBudget: 2_048 }
-
   let respText = await ai.models.generateContent({
-    model,
+    model: 'gemini-2.5-flash-preview-05-20',
     config,
     contents: [{
       role: 'user',
