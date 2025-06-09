e52db6a20979d6d7ee920b5b35d2cfa8b7fcf0c8
cassie <codef53@users.noreply.github.com>
2025-06-09 13:08:31 -0600

    refactor media acquisition and transcoding process

--- src/commands/download.ts
+++ src/commands/download.ts
@@ -1,7 +1,7 @@
 import type { CommandInteraction, CreateApplicationCommandOptions } from 'oceanic.js'
 import { Buffer } from 'node:buffer'
 import { readdir } from 'node:fs/promises'
-import { $, file as getFile } from 'bun'
+import { $, file } from 'bun'
 import { ApplicationCommandOptionTypes, ApplicationCommandTypes, Constants } from 'oceanic.js'
 import tryCatch from 'try-catch'
 
@@ -34,45 +34,62 @@ export async function handler(interaction: CommandInteraction) {
     files: [{ name: 'download.mp4', contents: media }],
   })
 }
-
-// technically I could avoid hardcoding this and use interaction.attachmentSizeLimit, but thats harder to debug...
 const MAX_FILE_SIZE = 10_000_000 // 10MB
+const TARGET_TOTAL_KB = (MAX_FILE_SIZE * 0.95 * 8) / 1_000
+const AUDIO_BITRATE_K = 96
 const DLP_FORMAT = 'bestvideo[height<=1080]+bestaudio/best[height<=1080]'
 async function acquireMedia(url: string): Promise<Error | Buffer> {
-  const destinationPrefix = Math.random().toString(36).slice(2) // USE THIS AS A PREFIX FOR ALL FILENAMES
+  const destinationPrefix = Math.random().toString(36).slice(2)
+  try {
+    // download
+    const dlpShellResp = await $`yt-dlp --format ${DLP_FORMAT} --playlist-items 1 --output ${destinationPrefix}.%\(ext\)s ${url}`.nothrow().quiet()
+    if (dlpShellResp.exitCode !== 0)
+      return new Error('failed to download media with yt-dlp')
+    const dlpFilename = await readdir('./').then(files => files.find(f => f.startsWith(destinationPrefix))) // file can have tons of extensions, determine what it's actual extension is
+    if (dlpFilename === undefined || dlpFilename === '')
+      return new Error('couldn\'t find file downloaded by yt-dlp')
+    const dlpFile = file(dlpFilename)
 
-  // download
-  const dlpShellResp = await $`yt-dlp --format ${DLP_FORMAT} --playlist-items 1 --output ${destinationPrefix}.%\(ext\)s ${url}`.nothrow().quiet()
-  if (dlpShellResp.exitCode !== 0)
-    return new Error('failed to download media with yt-dlp')
-  const dlpFilename = await readdir('./').then(files => files.find(f => f.startsWith(destinationPrefix))) // file can have tons of extensions, determine what it's actual extension is
-  if (dlpFilename === undefined || dlpFilename === '')
-    return new Error('couldn\'t find file downloaded by yt-dlp')
-  const dlpFile = getFile(dlpFilename)
+    // skip transcoding nonsense for images/audio
+    if (/\.(?:mp3|flac|opus|wav|png|jpg|jpeg|gif|webp)$/i.exec(dlpFilename) !== null) {
+      const buffer = Buffer.from(await dlpFile.arrayBuffer())
+      if (buffer.length > MAX_FILE_SIZE)
+        return new Error('output too big (>10MB)')
+      return buffer
+    }
 
-  // skip transcoding nonsense for images/audio
-  if (/\.(?:mp3|flac|opus|wav|png|jpg|jpeg|gif|webp)$/.exec(dlpFilename) !== null) {
-    const buffer = Buffer.from(await dlpFile.arrayBuffer())
-    dlpFile.delete()
+    // calculate target bitrate
+    const durationShellResp = await $`ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 ${dlpFilename}`.nothrow().quiet()
+    const duration = Number.parseFloat(durationShellResp.stdout.toString())
+    if (durationShellResp.exitCode !== 0 || Number.isNaN(duration) || duration <= 0)
+      return new Error('ffprobe failed to get media duration.')
+
+    const targetTotalBitrateK = TARGET_TOTAL_KB / duration
+    const targetVideoBitrateK = Math.floor(targetTotalBitrateK - AUDIO_BITRATE_K)
+    if (targetVideoBitrateK <= 0)
+      return new Error(`media cant fit within 10MB at a reasonable audio quality.`)
+
+    // 2 pass transcode
+    const ffmpegPass1 = await $`ffmpeg -y -i ${dlpFilename} -c:v libx264 -preset fast -b:v ${targetVideoBitrateK}k -pass 1 -passlogfile ${destinationPrefix}-passlog -an -f mp4 /dev/null`.nothrow().quiet()
+    if (ffmpegPass1.exitCode !== 0)
+      return new Error('ffmpeg transcoding pass 1 failed.')
+
+    const ffmpegPass2 = await $`ffmpeg -i ${dlpFilename} -c:v libx264 -preset fast -b:v ${targetVideoBitrateK}k -pass 2 -passlogfile ${destinationPrefix}-passlog -c:a aac -b:a ${AUDIO_BITRATE_K}k -movflags +faststart ${destinationPrefix}-FINAL.mp4`.nothrow().quiet()
+    if (ffmpegPass2.exitCode !== 0)
+      return new Error('ffmpeg transcoding pass 2 failed.')
+
+    const finalFile = file(`${destinationPrefix}-FINAL.mp4`)
+    const buffer = Buffer.from(await finalFile.arrayBuffer())
+
     if (buffer.length > MAX_FILE_SIZE)
-      return new Error('Output too big (>10MB)')
+      return new Error(`Transcoded output is still too big (${(buffer.length / 1_000_000).toFixed(2)}MB)`)
     return buffer
   }
-
-  // transcode to format known good for discord (x264 mp4)
-  // TODO: smarter transcode with math to fit into filesize more often
-  const ffmpegShellResp = await $`ffmpeg -y -i ${dlpFilename} -c:v libx264 -preset fast -crf 23 -c:a aac -b:a 128k -movflags +faststart ${destinationPrefix}-FINAL.mp4`.nothrow().quiet()
-  dlpFile.delete()
-  if (ffmpegShellResp.exitCode !== 0)
-    return new Error('failed to transcode media with ffmpeg')
-  const ffmpegFile = getFile(`${destinationPrefix}-FINAL.mp4`)
-
-  const buffer = Buffer.from(await ffmpegFile.arrayBuffer())
-  ffmpegFile.delete()
-  if (buffer.length > MAX_FILE_SIZE)
-    return new Error('Output too big (>10MB)')
-
-  return buffer
+  finally {
+    // cleanup!
+    const processFiles = await readdir('./').then(files => files.filter(f => f.startsWith(destinationPrefix)))
+    processFiles.forEach(f => file(f).delete())
+  }
 }
 
 async function validateMedia(url: string): Promise<void | Error> {
