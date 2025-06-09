fd1230e486dec70b3b733f48227d15907f8ca4af
cassie <codef53@users.noreply.github.com>
2025-06-09 15:28:03 -0600

    add custom reddit support (gallery only atm)

--- src/commands/download.ts
+++ src/commands/download.ts
@@ -5,6 +5,8 @@ import { $, file, randomUUIDv7 } from 'bun'
 import { ApplicationCommandOptionTypes, ApplicationCommandTypes, Constants } from 'oceanic.js'
 import tryCatch from 'try-catch'
 
+interface BufferAndFiletype { buffer: Buffer, filetype: string }
+
 export const description: CreateApplicationCommandOptions = {
   name: 'download',
   description: 'download a media url',
@@ -19,26 +21,84 @@ export const description: CreateApplicationCommandOptions = {
 }
 
 export async function handler(interaction: CommandInteraction) {
+  const url = interaction.data.options.getStringOption('url', true)!.value.trim()
+  if (!URL.canParse(url))
+    return interaction.reply({ content: '⚠️ Invalid URL provided', flags: Constants.MessageFlags.EPHEMERAL })
+
   await interaction.reply({ content: 'downloading...', flags: Constants.MessageFlags.EPHEMERAL })
 
-  const url = interaction.data.options.getStringOption('url', true)!.value.trim()
-  const mediaError = await validateMedia(url)
-  if (Error.isError(mediaError))
-    return interaction.editOriginal({ content: `⚠️ ${mediaError.message}` })
+  const fileBuffers: BufferAndFiletype[] = []
 
-  const media = await acquireMedia(url)
-  if (Error.isError(media))
-    return interaction.editOriginal({ content: `⚠️ ${media.message}` })
+  let mediaAcquisitioner = acquireMediaDLP
+  if (url.includes('reddit.com') || url.includes('redd.it'))
+    mediaAcquisitioner = acquireRedditMedia
 
-  return interaction.createFollowup({
-    files: [{ name: `${randomUUIDv7('base64url')}.mp4`, contents: media }],
-  })
+  const acquisitionerResult = await mediaAcquisitioner(url)
+  if (Error.isError(acquisitionerResult))
+    return interaction.editOriginal({ content: `⚠️ ${acquisitionerResult.message}` })
+  else
+    fileBuffers.push(...acquisitionerResult)
+
+  const chunkedFileBuffers: BufferAndFiletype[][] = []
+  for (let i = 0; i < fileBuffers.length; i += 10)
+    chunkedFileBuffers.push(fileBuffers.slice(i, i + 10))
+  for (const fileBufferChunk of chunkedFileBuffers) {
+    await interaction.createFollowup({
+      files: fileBufferChunk.map(({ buffer, filetype }) => ({ name: `${randomUUIDv7('base64url')}.${filetype}`, contents: buffer })),
+    })
+  }
 }
+
+// fix for reddit galleries & gifs
+async function acquireRedditMedia(url: string): Promise<Error | BufferAndFiletype[]> {
+  // TODO: handle /r/*/s/* urls, ex https://reddit.com/r/YouShouldKnow/s/l6p7VF51z7
+  if (/\/s\//.exec(url) !== null)
+    return new Error('reddit share urls `https://reddit.com/r/.../s/...` aren\'t supported')
+
+  const postId = /(?<=\/)\w{3,10}$|(?<=comments\/)\w{3,10}/.exec(url)?.[0]
+  if (postId === null)
+    return new Error('failed to acquire reddit post id')
+
+  const postData: any | null = await fetch(`https://api.reddit.com/${postId}.json`)
+    .then<any>(r => r.json())
+    .then(r => r[0].data.children[0].data)
+    .catch(() => null)
+  if (postData === null)
+    return new Error('failed to get reddit post metadata')
+
+  // fallback on yt-dlp for reddit videos
+  if (postData.is_video)
+    return acquireMediaDLP(url)
+
+  if (postData.is_gallery) {
+    const imageUrls = Object.values(postData.media_metadata)
+      .map((item: any) => (item.s.u as string).replace('preview', 'i').replace(/\?.*$/, ''))
+    const imageBuffers = await Promise.all([...imageUrls.map(url => imgUrlToBuffer(url))])
+    if (imageBuffers.some(Error.isError))
+      return new Error('failed to download gallery images')
+    return imageBuffers as BufferAndFiletype[]
+  }
+
+  // posts that are normal text, or single image end up here, filter out normal text first
+  // example of text post data: https://api.reddit.com/1i5bx0d.json, this should error
+  // example of single image post data: https://api.reddit.com/1l6w06k.json, this should respond with https://i.redd.it/mihhvbmazt5f1.gif (in postData.url)
+  return new Error('temp cant be assed to implement rn')
+}
+async function imgUrlToBuffer(url: string): Promise<BufferAndFiletype | Error> {
+  const arrayBuffer = await fetch(url).then(r => r.arrayBuffer()).catch(() => null)
+  if (arrayBuffer === null) return new Error('failed to download image')
+  return { buffer: Buffer.from(arrayBuffer), filetype: url.split('.').at(-1)! }
+}
+
 const MAX_FILE_SIZE = 10_000_000 // 10MB
-const TARGET_TOTAL_KB = (MAX_FILE_SIZE * 0.95 * 8) / 1_000
+const TARGET_TOTAL_KB = (MAX_FILE_SIZE * 0.9 * 8) / 1_000
 const AUDIO_BITRATE_K = 96
 const DLP_FORMAT = 'bestvideo[height<=1080]+bestaudio/best[height<=1080]'
-async function acquireMedia(url: string): Promise<Error | Buffer> {
+async function acquireMediaDLP(url: string): Promise<Error | BufferAndFiletype[]> {
+  const mediaError = await validateMediaDLP(url)
+  if (Error.isError(mediaError))
+    return mediaError
+
   const destinationPrefix = randomUUIDv7('base64url')
   try {
     // download
@@ -55,7 +115,7 @@ async function acquireMedia(url: string): Promise<Error | Buffer> {
       const buffer = Buffer.from(await dlpFile.arrayBuffer())
       if (buffer.length > MAX_FILE_SIZE)
         return new Error('output too big (>10MB)')
-      return buffer
+      return [{ buffer, filetype: dlpFilename.split('.').at(-1)! }]
     }
 
     // calculate target bitrate
@@ -82,8 +142,8 @@ async function acquireMedia(url: string): Promise<Error | Buffer> {
     const buffer = Buffer.from(await finalFile.arrayBuffer())
 
     if (buffer.length > MAX_FILE_SIZE)
-      return new Error(`Transcoded output is still too big (${(buffer.length / 1_000_000).toFixed(2)}MB)`)
-    return buffer
+      return new Error(`transcoded output is still too big (${(buffer.length / 1_000_000).toFixed(2)}MB)`)
+    return [{ buffer, filetype: 'mp4' }]
   }
   finally {
     // cleanup!
@@ -92,10 +152,7 @@ async function acquireMedia(url: string): Promise<Error | Buffer> {
   }
 }
 
-async function validateMedia(url: string): Promise<void | Error> {
-  if (!URL.canParse(url))
-    return new Error('Invalid URL provided')
-
+async function validateMediaDLP(url: string): Promise<void | Error> {
   const shellResponse = await $`yt-dlp --no-warnings --dump-single-json --playlist-items 1 ${url}`.nothrow().quiet()
 
   if (shellResponse.exitCode !== 0 || shellResponse.stdout.length === 0) {
