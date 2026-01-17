0bf170568ab0cde7daf362a6572bb94120b2df3c
Cassie <37855219+CodeF53@users.noreply.github.com>
2026-01-16 17:27:52 -0700

    switch to gemini v3

--- src/commands/gemini.ts
+++ src/commands/gemini.ts
@@ -71,7 +71,7 @@ export async function handler(interaction: CommandInteraction) {
   }
   const runtimeStart = performance.now()
   let respText = await ai.models.generateContent({
-    model: 'gemini-2.5-flash',
+    model: 'gemini-3-flash-preview',
     config,
     contents: [{
       role: 'user',
